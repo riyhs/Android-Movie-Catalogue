fe0670dd8584960d2780f331c8a1cf396b4e18e4
Riyaldi Hasan <riy.2923@gmail.com>
2021-01-25 14:14:13 +0700

    add binding

--- app/src/main/java/com/riyaldi/moviecatalogue/ui/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/riyaldi/moviecatalogue/ui/favorite/FavoriteFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.google.android.material.tabs.TabLayoutMediator
 import com.riyaldi.moviecatalogue.R
+import com.riyaldi.moviecatalogue.databinding.FragmentFavoriteBinding
 import com.riyaldi.moviecatalogue.ui.favorite.movie.MovieFavoriteFragment
 import com.riyaldi.moviecatalogue.ui.favorite.tvshow.TvShowFavoriteFragment
 import com.riyaldi.moviecatalogue.ui.home.ViewpagerAdapter
@@ -14,11 +15,15 @@ import kotlinx.android.synthetic.main.fragment_favorite.*
 
 class FavoriteFragment : Fragment() {
 
+    private var _favoriteFragmentBinding: FragmentFavoriteBinding? = null
+    private  val binding get() = _favoriteFragmentBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_favorite, container, false)
+        _favoriteFragmentBinding = FragmentFavoriteBinding.inflate(layoutInflater, container, false)
+        return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -33,10 +38,15 @@ class FavoriteFragment : Fragment() {
         val fragmentList = listOf(MovieFavoriteFragment(), TvShowFavoriteFragment())
         val tabTitle = listOf(resources.getString(R.string.movie), resources.getString(R.string.tv_show))
 
-        viewpager.adapter = ViewpagerAdapter(fragmentList, requireActivity().supportFragmentManager, lifecycle)
+        binding?.viewpager?.adapter = ViewpagerAdapter(fragmentList, requireActivity().supportFragmentManager, lifecycle)
 
         TabLayoutMediator(tabLayout2, viewpager){tab, position ->
             tab.text = tabTitle[position]
         }.attach()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _favoriteFragmentBinding = null
     }
 }
